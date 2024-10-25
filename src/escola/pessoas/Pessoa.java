e01d2f30d0d1cb6c5802f60b2438ad363f14cc46
Eduardo Fabri <eduardohfabri@gmail.com>
2024-10-25 11:19:15 -0300

    Implementar a classe abstrata Pessoa

---
+++ src/escola/pessoas/Pessoa.java
@@ -0,0 +1,85 @@
+package escola.pessoas;
+
+import escola.biblioteca.Livro;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public abstract class Pessoa {
+    private String nome;
+    private String CPF;
+    private LocalDate dataNascimento;
+    private String endereco;
+    private ArrayList<Livro> livrosEmprestados;
+
+    public Pessoa() {
+        this.livrosEmprestados = new ArrayList<>();
+    }
+
+    public Pessoa(String nome, String CPF, LocalDate dataNascimento, String endereco) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+        this.livrosEmprestados = new ArrayList<>();
+    }
+
+    public String exibirInformacoes() {
+        return "Nome: " + getNome() +
+                ", CPF: " + getCPF() +
+                ", Nascimento: " + getDataNascimento() +
+                ", endereço: " + getEndereco();
+    }
+
+    public void emprestarLivro(Livro livro) {
+        if (livro != null) {
+            livrosEmprestados.add(livro);
+            System.out.println("Livro emprestado: " + livro.getTitulo());
+        } else {
+            System.out.println("Livro não pode ser nulo.");
+        }
+    }
+
+    public void devolverLivro(Livro livro) {
+        if (livrosEmprestados.remove(livro)) {
+            System.out.println("Livro devolvido: " + livro.getTitulo());
+        } else {
+            System.out.println("Este livro não está emprestado.");
+        }
+    }
+
+    public ArrayList<Livro> getLivrosEmprestados() {
+        return livrosEmprestados;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
