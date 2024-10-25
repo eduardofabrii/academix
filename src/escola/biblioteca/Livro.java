42cfb5d2acb15e2c4b1f12b2330609ae56346d98
Eduardo Fabri <eduardohfabri@gmail.com>
2024-10-25 11:19:46 -0300

    Implementar a classe Livro

---
+++ src/escola/biblioteca/Livro.java
@@ -0,0 +1,40 @@
+package escola.biblioteca;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicado;
+
+    public Livro() {
+    }
+
+    public Livro(String titulo, String autor, int anoPublicado) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicado = anoPublicado;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicado() {
+        return anoPublicado;
+    }
+
+    public void setAnoPublicado(int anoPublicado) {
+        this.anoPublicado = anoPublicado;
+    }
+}
