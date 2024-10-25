19176dc5415ec3c287c364123e342386f534051c
Eduardo Fabri <eduardohfabri@gmail.com>
2024-10-25 11:24:13 -0300

    Implementar a classe abstrata Funcionario

---
+++ src/escola/pessoas/Funcionario.java
@@ -0,0 +1,22 @@
+package escola.pessoas;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public abstract class Funcionario extends Pessoa {
+    private static int contador = 0;
+    private int codigo;
+
+    public Funcionario() {
+        this.codigo = ++contador;
+    }
+
+    public int getIdade() {
+        LocalDate hoje = LocalDate.now();
+        return Period.between(getDataNascimento(), hoje).getYears();
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+}
