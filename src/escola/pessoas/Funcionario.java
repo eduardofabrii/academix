43756ac1b138df45b72b05f0007e33dea589a8d5
Eduardo Fabri <eduardohfabri@gmail.com>
2024-10-26 18:32:28 -0300

    Implementar a classe abstrata Funcionário

--- src/escola/pessoas/Funcionario.java
+++ src/escola/pessoas/Funcionario.java
@@ -1,17 +1,43 @@
 package escola.pessoas;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public abstract class Funcionario extends Pessoa {
     private static int contador = 0;
     private int codigo;
     private double salario;
+    private List<String> registrosPonto;
 
-
-
-    public Funcionario() {
+    public Funcionario(String nome, String CPF, LocalDate dataNascimento, String endereco) {
+        super(nome, CPF, dataNascimento, endereco);
         this.codigo = ++contador;
+    }
+
+    public String baterPontoEntrada(Pessoa pessoa) {
+        String mascara = "dd 'de' MMMM 'de' yyyy ' | Horário:' HH:mm";
+        SimpleDateFormat sdf = new SimpleDateFormat(mascara);
+
+        String registro = pessoa.getNome() + " registrou ponto de entrada em " + sdf.format(new java.util.Date());
+
+        setRegistrosPonto(Collections.singletonList(registro));
+
+        return registro;
+    }
+
+    public String baterPontoSaida(Pessoa pessoa) {
+        String mascara = "dd 'de' MMMM 'de' yyyy ' | Horário:' HH:mm";
+        SimpleDateFormat sdf = new SimpleDateFormat(mascara);
+
+        String registro = pessoa.getNome() + " registrou ponto de saída em " + sdf.format(new java.util.Date());
+
+        setRegistrosPonto(Collections.singletonList(registro));
+
+        return registro;
     }
 
     public int getIdade() {
@@ -21,5 +47,13 @@ public abstract class Funcionario extends Pessoa {
 
     public int getCodigo() {
         return codigo;
+    }
+
+    public List<String> getRegistrosPonto() {
+        return registrosPonto;
+    }
+
+    public void setRegistrosPonto(List<String> registrosPonto) {
+        this.registrosPonto = registrosPonto;
     }
 }
