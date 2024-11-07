b5e6880a9b32b67feb0831433aa1b13ae955f97f
Renan <renan.herculano@pucpr.edu.br>
2024-11-07 15:19:09 -0300

    ConexaoFaxineiro
    Adicionei codigo para salvar em txt no minibanco

--- src/escola/UI/cadastro/pessoas/CadastroFaxineiroUI.java
+++ src/escola/UI/cadastro/pessoas/CadastroFaxineiroUI.java
@@ -1,10 +1,12 @@
 package escola.UI.cadastro.pessoas;
 
+import escola.minibanco.pessoas.PessoaTxt;
 import escola.pessoas.Faxineiro;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class CadastroFaxineiroUI {
@@ -32,6 +34,12 @@ public class CadastroFaxineiroUI {
                 double salario = Double.parseDouble(salarioTextField.getText());
 
                 Faxineiro novoFaxineiro = new Faxineiro(nome, cpf, dataNascimento, endereco, 0, salario);
+
+                try{
+                    PessoaTxt.salvarPessoa(novoFaxineiro);
+                } catch(IOException i){
+                    System.out.println("Erro: Faxineiro não salvo");
+                }
 
                 System.out.println(novoFaxineiro.exibirInformacoes());
 
