53150020f04453ab86fb5ef79534b6bff24e89a6
Renan <renan.herculano@pucpr.edu.br>
2024-11-07 15:16:06 -0300

    TesteParaTxt

--- src/escola/minibanco/pessoas/Teste.java
+++ src/escola/minibanco/pessoas/Teste.java
@@ -10,7 +10,7 @@ public class Teste {
             Pessoa pessoa = PessoaTxt.carregarPessoa();
             System.out.println(pessoa);
         } catch (IOException i){
-            System.out.println("Erro no salvamento de arquivo");
+            System.out.println("Arquivo não encontrado");
         } catch (ClassNotFoundException c){
             c.getStackTrace();
             System.out.println("Classe não encontrada");
