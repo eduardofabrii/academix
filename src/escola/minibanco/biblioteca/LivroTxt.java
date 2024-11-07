0964fdc7d10da97ed87e20cd46abeb047faabc21
Renan <renan.herculano@pucpr.edu.br>
2024-11-07 15:30:32 -0300

    CriacaoTxtBiblioteca
    Criado arquivo e pasta para armazenar o arquivo txt com o objeto livro

---
+++ src/escola/minibanco/biblioteca/LivroTxt.java
@@ -0,0 +1,27 @@
+package escola.minibanco.biblioteca;
+
+import escola.biblioteca.Livro;
+
+import java.io.*;
+
+public class LivroTxt {
+    public static void salvarLivro(Livro l) throws IOException {
+        FileOutputStream fos = new FileOutputStream("escola/minibanco/biblioteca/livro.txt");
+        ObjectOutputStream os = new ObjectOutputStream(fos);
+        os.writeObject(l);
+
+        os.close();
+        fos.close();
+    }
+
+    public static Livro carregarLivro() throws IOException, ClassNotFoundException{
+        FileInputStream fis = new FileInputStream("escola/minibanco/biblioteca/livro.txt");
+        ObjectInputStream is = new ObjectInputStream(fis);
+        Livro livro = (Livro) is.readObject();
+
+        is.close();
+        fis.close();
+
+        return livro;
+    }
+}
